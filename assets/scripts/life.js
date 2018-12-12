aa2956a507929053b4e946386c78e01eaa3cda43
naturalbornchiller <mail@emmanuelprice.com>
2018-12-11 19:07:34 -0500

    Adds neighbor testing functionality.

--- assets/scripts/life.js
+++ assets/scripts/life.js
@@ -1,5 +1,47 @@
 
+    // if (isFilled(x-1, y-1)) neighbors++
+    // if (isFilled(x-1, y)) neighbors++
+    // if (isFilled(x-1, y+1)) neighbors++
+    // if (isFilled(x, y-1)) neighbors++
+    // if (isFilled(x, y+1)) neighbors++
+    // if (isFilled(x+1, y-1)) neighbors++
+    // if (isFilled(x+1, y)) neighbors++
+    // if (isFilled(x+1, y+1)) neighbors++
+
+
 const canvas = $('#c').getContext('2d')
-const cells = []
+const currentGrid = []
 canvas.strokeStyle = '#e1e1e1'
 canvas.fillStyle = 'cadetBlue'
+
+const isFilled = (x, y) => cells[x] && cells[x][y]
+const countNeighbors = (x, y) => {
+    const neighbors = [isFilled(x-1, y-1), isFilled(x-1, y), isFilled(x-1, y+1), isFilled(x, y-1), 
+                    isFilled(x, y+1), isFilled(x+1, y-1), isFilled(x+1, y), isFilled(x+1, y+1)]
+    return neighbors.filter(neighbor => neighbor === true).length
+}
+
+const deadOrAlive = (cell, count) => {
+
+}
+
+const updateGrid = () => {
+    const nextGrid = []
+    cells.forEach((row, x) => {
+        nextGrid[x] = []
+        row.forEach((cell, y) => {
+            const count = countNeighbors(x, y)
+            nextGrid[x][y] = deadOrAlive(cell, count)
+        })
+    })
+}
+
+const init = (height=64, width=64) => {
+    for (let i = 0; i < width; i++) {
+        cells[i] = []
+        for (let j = 0; j < height; j++) {
+            cells[i][j] = !!Math.floor(Math.random())
+        }
+    }
+    updateGrid()
+}
