4033e7663ab93c6080f2279b61df6bc986f2bde5
naturalbornchiller <mail@emmanuelprice.com>
2018-12-11 22:32:25 -0500

    Completes logic
    Need to work out kinks

--- assets/scripts/life.js
+++ assets/scripts/life.js
@@ -1,47 +1,49 @@
-
-    // if (isFilled(x-1, y-1)) neighbors++
-    // if (isFilled(x-1, y)) neighbors++
-    // if (isFilled(x-1, y+1)) neighbors++
-    // if (isFilled(x, y-1)) neighbors++
-    // if (isFilled(x, y+1)) neighbors++
-    // if (isFilled(x+1, y-1)) neighbors++
-    // if (isFilled(x+1, y)) neighbors++
-    // if (isFilled(x+1, y+1)) neighbors++
-
-
-const canvas = $('#c').getContext('2d')
-const currentGrid = []
+const canvas = document.getElementById('c').getContext('2d')
 canvas.strokeStyle = '#e1e1e1'
 canvas.fillStyle = 'cadetBlue'
+let cells = []
 
 const isFilled = (x, y) => cells[x] && cells[x][y]
+const deadOrAlive = (cell, count) => (cell ? (count === 2 || count === 3) : count === 3)
 const countNeighbors = (x, y) => {
     const neighbors = [isFilled(x-1, y-1), isFilled(x-1, y), isFilled(x-1, y+1), isFilled(x, y-1), 
                     isFilled(x, y+1), isFilled(x+1, y-1), isFilled(x+1, y), isFilled(x+1, y+1)]
     return neighbors.filter(neighbor => neighbor === true).length
 }
 
-const deadOrAlive = (cell, count) => {
-
-}
-
-const updateGrid = () => {
-    const nextGrid = []
+const draw = (width=600, height=400) => {
+    canvas.clearRect(0, 0, width, height)
     cells.forEach((row, x) => {
-        nextGrid[x] = []
         row.forEach((cell, y) => {
-            const count = countNeighbors(x, y)
-            nextGrid[x][y] = deadOrAlive(cell, count)
+            canvas.beginPath()
+            canvas.rect(x*8, y*8, 8, 8)
+            if (cell) {
+                canvas.fill()
+            } else {
+                canvas.stroke()
+            }
         })
     })
+    setTimeout(() => update(), 70)
 }
 
-const init = (height=64, width=64) => {
+const update = () => {
+    cells = cells.map((row, x) => {
+        return cells[x] = row.map((cell, y) => {
+            return cells[x][y] = deadOrAlive(cell, countNeighbors(x, y))
+        })
+    })
+    draw()
+}
+
+const init = (width=64, height=64) => {
     for (let i = 0; i < width; i++) {
         cells[i] = []
         for (let j = 0; j < height; j++) {
             cells[i][j] = !!Math.floor(Math.random())
         }
     }
-    updateGrid()
+    update()
 }
+
+module.exports = init
